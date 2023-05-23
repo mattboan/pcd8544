d8eafc8a6466ac646966a6e5f2e7eae577572417
mattboan <matt.n.boan@gmail.com>
2023-05-23 19:53:05 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# PCD8544 Component for ESP-IDF
+
+This is an ESP-IDF component for controlling a PCD8544-based LCD display. The PCD8544 is commonly used with Nokia 5110 LCD displays and provides a simple interface for displaying text and graphics.
+
+Currently a work in progress, but we can draw lines, set pixels, and started the implementation for rectangles, and circles.
+
+After that I will start working on drawing bitmaps and characters.
+
+Check out the splash screen:
+
+![Example Image](https://www.voidstudios.com.au/_next/image?url=https%3A%2F%2Fxovjhtjkytzursvsbvbp.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fheader_img%2Fproject-b89c1cc6-cffa-44ba-a650-d4ba90e11770.undefined&w=1080&q=75)
+
+## Getting Started
+
+To use this component in your ESP-IDF project, follow these steps:
+
+1. Clone or download this repository into the `components` directory of your ESP-IDF project.
+
+2. In your project's main folder `CMakeLists.txt` file, add the following line to include the component:
+
+    ```cmake
+    idf_component_register(SRCS "<your-main and other files>.c"
+                       INCLUDE_DIRS "."
+                       REQUIRES pcd8544
+    ```
+
+3. In your application code, include the PCD8544 header file:
+
+    ```c
+    #include "pcd8544.h"
+    ```
+
+4. Initialize the PCD8544 display using the provided functions:
+
+    ```c
+    spi_device_handle_t handle = init_lcd();
+
+    // Setup the pins for output mode
+    gpio_set_direction(RESET, GPIO_MODE_OUTPUT);
+    gpio_set_direction(DC, GPIO_MODE_OUTPUT);
+
+    // Reset the dispaly
+    gpio_set_level(RESET, 1);
+    gpio_set_level(RESET, 0);
+    gpio_set_level(RESET, 1);
+
+    // Set the contrast
+    send_command(handle, PCD8544_FUNCTIONSET | PCD8544_EXTENDEDINSTRUCTION);
+    send_command(handle, PCD8544_SETVOP | 30); // Contrast
+    send_command(handle, PCD8544_FUNCTIONSET);
+
+    // Set the Voltage Bias
+    send_command(handle, PCD8544_FUNCTIONSET | PCD8544_EXTENDEDINSTRUCTION);
+    send_command(handle, PCD8544_SETBIAS | 0x07); // Bias
+    send_command(handle, PCD8544_FUNCTIONSET);
+
+    // Sets the LCD to normal mode
+    send_command(handle, PCD8544_FUNCTIONSET);
+
+    // Set the display to Normal mode
+    send_command(handle, PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYNORMAL);
+
+    // Draws the first frame
+    draw_frame_buffer(handle);
+
+    ```
+
+5. Use the available functions to control the display, such as:
+
+    ```c
+    clear_frame_buffer(handle);
+
+    draw_line(handle, 0, 0, 20, 20);
+
+    set_pixel(handle, 30, 30);
+    ```
+
+6. Build and flash your ESP-IDF project to see the output on the PCD8544 display.
+
+Please refer to the header file `pcd8544.h` for detailed documentation on each function and its usage, aswell as changing some config options; pin outs, defualt buffer init, frame buffer size, and queue size.
+
+## License
+
+This component is licensed under the [Apache License](LICENSE).
---
+++ include/pcd8544.h
@@ -0,0 +1,157 @@
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "esp_log.h"
+
+
+// Function set, Power down mode
+#define PCD8544_POWERDOWN 0x04 
+
+// Function set, Entry mode
+#define PCD8544_ENTRYMODE 0x02 
+
+//Function set, Extended instruction set control
+#define PCD8544_EXTENDEDINSTRUCTION 0x01 
+
+// Display control, blank
+#define PCD8544_DISPLAYBLANK 0x0    
+// Display control, normal mode
+#define PCD8544_DISPLAYNORMAL 0x4
+// Display control, all segments on   
+#define PCD8544_DISPLAYALLON 0x1    
+// Display control, inverse mode
+#define PCD8544_DISPLAYINVERTED 0x5 
+// Basic instruction set
+#define PCD8544_FUNCTIONSET 0x20
+// Basic instruction set - Set display configuration 
+#define PCD8544_DISPLAYCONTROL 0x08
+// Basic instruction set - Set Y address of RAM, 0 <= Y <= 5 
+#define PCD8544_SETYADDR 0x40 
+// Basic instruction set - Set X address of RAM, 0 <= X <= 83
+#define PCD8544_SETXADDR0x80 
+// Extended instruction set - Set temperature coefficient
+#define PCD8544_SETTEMP 0x04 
+// Extended instruction set - Set bias system
+#define PCD8544_SETBIAS 0x10 
+// Extended instruction set - Write Vop to register
+#define PCD8544_SETVOP 0x80  
+
+// LCD Width and Height
+#define LCD_WIDTH 84
+#define LCD_HEIGHT 48
+
+// Pin Selection
+#define DC 4
+#define RESET 2
+#define MOSI 23
+#define SCLK 18
+
+// SPI Config
+#define FRAME_BUFFER_SIZE 504
+#define QUEUE_SIZE 32
+
+/**
+ * @brief Delays execution for the specified number of milliseconds.
+ * 
+ * TODO: Move this to another component: esp32-time
+ *
+ * @param ms The delay duration in milliseconds.
+ */
+void delay(int ms);
+
+
+/**
+ * @brief Initializes the SPI interface for the LCD.
+ *
+ * @return The SPI device handle for the LCD.
+ */
+spi_device_handle_t init_lcd();
+
+
+/**
+ * @brief Sends a command to the LCD.
+ *
+ * @param handle The SPI device handle for the LCD.
+ * @param command The command to be sent.
+ */
+void send_command(spi_device_handle_t handle, uint8_t command);
+
+
+/**
+ * @brief Sends data to the LCD.
+ *
+ * @param handle The SPI device handle for the LCD.
+ * @param data The data to be sent.
+ */
+void send_data(spi_device_handle_t handle, uint8_t data);
+
+
+/**
+ * @brief Clears the frame buffer.
+ *
+ * @param handle The SPI device handle for the LCD.
+ */
+void clear_frame_buffer(spi_device_handle_t handle);
+
+
+/**
+ * @brief Sets the color of a pixel at the specified coordinates.
+ *
+ * @param handle The SPI device handle for the LCD.
+ * @param x The x-coordinate of the pixel.
+ * @param y The y-coordinate of the pixel.
+ * @param color The color of the pixel (true for white, false for black).
+ */
+void set_pixel(spi_device_handle_t handle, uint8_t x, uint8_t y, bool color);
+
+
+/**
+ * @brief Draws a line between two points.
+ *
+ * @param handle The SPI device handle for the LCD.
+ * @param x0 The x-coordinate of the starting point.
+ * @param y0 The y-coordinate of the starting point.
+ * @param x1 The x-coordinate of the ending point.
+ * @param y1 The y-coordinate of the ending point.
+ * @param color The color of the line (true for white, false for black).
+ */
+void draw_line(spi_device_handle_t handle, uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, bool color);
+
+
+/**
+ * @brief Draws a circle with the specified center, radius, and color.
+ *
+ * @param handle The SPI device handle for the LCD.
+ * @param x0 The x-coordinate of the center of the circle.
+ * @param y0 The y-coordinate of the center of the circle.
+ * @param r The radius of the circle.
+ * @param color The color of the circle (true for white, false for black).
+ */
+void draw_circle(spi_device_handle_t handle, uint8_t x0, uint8_t y0, uint8_t r, bool color);
+
+
+/**
+ * @brief Draws a rectangle with the specified coordinates and color.
+ *
+ * @param handle The SPI device handle for the LCD.
+ * @param x0 The x-coordinate of the top-left corner of the rectangle.
+ * @param y0 The y-coordinate of the top-left corner of the rectangle.
+ * @param x1 The x-coordinate of the bottom-right corner of the rectangle.
+ * @param y1 The y-coordinate of the bottom-right corner of the rectangle.
+ * @param color The color of the rectangle (true for white, false for black).
+ */
+void draw_rect(spi_device_handle_t handle, uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, bool color);
+
+
+/**
+ * @brief Draws the frame buffer on the LCD.
+ *
+ * @param handle The SPI device handle for the LCD.
+ */
+void draw_frame_buffer(spi_device_handle_t handle);
